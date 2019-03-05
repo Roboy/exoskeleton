01f75fca72a1c0d108e3c518ea1dd6e172e931d7
justDabuK <kevinjust87@gmail.com>
2019-03-05 16:24:09 +0100

    some script updates, and an new version without joints

--- scripts/GetJointInfo/GetJointInfo.py
+++ scripts/GetJointInfo/GetJointInfo.py
@@ -1,5 +1,5 @@
-#Author-
-#Description-
+# Author-
+# Description-
 
 import adsk.core
 import adsk.fusion
@@ -10,17 +10,16 @@ from collections import defaultdict
 
 def run(context):
     ui = None
-    
+
     try:
         app = adsk.core.Application.get()
         ui = app.userInterface
-        #ui.messageBox('Hello script')
+        # ui.messageBox('Hello script')
         # get active design
         product = app.activeProduct
         design = adsk.fusion.Design.cast(product)
         all_components = design.allComponents
-        allRigidGroups = design.rootComponent.allRigidGroups
-        ui.messageBox('trying to get the components')
+        rootOcc = design.rootComponent.occurrences
         with open("/Users/Kevin/Documents/Uni/RCI/Roboy/git_repos/exoskeleton/geometry.txt", "w") as geometry_file:
             for com in all_components:
                 if com is not None:
@@ -31,11 +30,30 @@ def run(context):
                         if joi is not None:
                             vec = adsk.fusion.RevoluteJointMotion.cast(joi.jointMotion).rotationAxisVector.asPoint()
 
-                            ui.messageBox("As-Built Joint: " + joi.name +
-                                          "\n rotationalAxisVector.asPoint() :  " + str(vec.asArray()) +
-                                          "\n geometry : " + str(joi.geometry.origin.asArray()))
-                            ui.messageBox("attributes: " + str(joi.objectType))
-                            geometry_file.write(joi.name + " geometry : " + str(joi.geometry.origin.asArray()) + "\n")
+                            # ui.messageBox("Component: " + com.name +
+                            #               "\n As-Built Joint: " + joi.name +
+                            #               "\n rotationalAxisVector.asPoint() :  " + str(vec.asArray()) +
+                            #               "\n geometry : " + str(joi.geometry.origin.asArray()))
+                            # geometry_file.write(joi.name + " geometry : " + str(joi.geometry.origin.asArray()) + "\n")
+
+                    all_rigids = com.rigidGroups
+
+                    # for rigid in all_rigids:
+                    #     if rigid is not None:
+                    #         for occ in rigid.occurrences:
+                    #
+                    #             ui.messageBox("Component: " + com.name +
+                    #                           "\n RigidGroup: " + rigid.name +
+                    #                           "\n Occurence: " + occ.name +
+                    #                           "\n transform Matrix: " + str(occ.transform.translation.asArray()))
+
+            for occ in rootOcc:
+                if occ is not None:
+                    ui.messageBox("Occurrence: " + occ.name +
+                                  "\n x: " + str(occ.transform.translation.x * 0.01) +
+                                  "\n y: " + str(occ.transform.translation.y * 0.01) +
+                                  "\n z: " + str(occ.transform.translation.z * 0.01))
+
 
 
     except:
